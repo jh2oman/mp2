80ab38650e6f7b785515b07052e9c8907e0f47bb
Jacob Waterman <jwaterm2@illinois.edu>
2015-03-13 03:34:25 +0000

    fixed partials

--- js/app1.js
+++ js/app1.js
@@ -10,19 +10,19 @@ myApp.run(function($rootScope) {
 myApp.config(['$routeProvider', function($routeProvider){
 	$routeProvider.
 	when('/list', {
-		templateUrl: '/partials/list.html',
+		templateUrl: './partials/list.html',
 		controller: 'ListController'
 	}).
 	when('/details/:imdbID', {
-		templateUrl: '/partials/details.html',
+		templateUrl: './partials/details.html',
 		controller: 'DetailsController'
 	}).
 	when('/gallery', {
-		templateUrl: '/partials/gallery.html',
+		templateUrl: './partials/gallery.html',
 		controller: 'GalleryController'
 	}).
 	when('/picker', {
-		templateUrl: '/partials/picker.html',
+		templateUrl: './partials/picker.html',
 		controller: 'PickerController'
 	}).
 	otherwise({
